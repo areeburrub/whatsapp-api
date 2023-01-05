285e709af73df256825d18ad6f6b02c54cd5ec54
areeburrub <areeburrub@gmail.com>
2023-01-05 23:05:40 +0530

    debugging webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -4,7 +4,31 @@ import axios from 'axios';
 const token = process.env.WHATSAPP_TOKEN;
 
 export default function handler(req, res) {
-  if (req.method == 'POST') {
+  if (req.method == 'GET') {
+
+    const verify_token = process.env.VERIFY_TOKEN;
+
+    // Parse params from the webhook verification request
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    // Check if a token and mode were sent
+    if (mode && token) {
+      // Check the mode and token sent are correct
+      if (mode === "subscribe" && token === verify_token) {
+        // Respond with 200 OK and challenge token from the request
+        console.log("WEBHOOK_VERIFIED");
+        res.status(200).send(challenge);
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.status(403);
+      }
+    }
+   
+  }
+  else{
+
     let body = req.body;
     console.log(req);
     // Check the Incoming webhook message
@@ -44,26 +68,5 @@ export default function handler(req, res) {
     //   // Return a '404 Not Found' if event is not from a WhatsApp API
     //   res.status(404).json({ data: 'error' });
     // }
-  }
-  else{
-    const verify_token = process.env.VERIFY_TOKEN;
-
-    // Parse params from the webhook verification request
-    let mode = req.query["hub.mode"];
-    let token = req.query["hub.verify_token"];
-    let challenge = req.query["hub.challenge"];
-
-    // Check if a token and mode were sent
-    if (mode && token) {
-      // Check the mode and token sent are correct
-      if (mode === "subscribe" && token === verify_token) {
-        // Respond with 200 OK and challenge token from the request
-        console.log("WEBHOOK_VERIFIED");
-        res.status(200).send(challenge);
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.status(403);
-      }
-    }
   }
 }
