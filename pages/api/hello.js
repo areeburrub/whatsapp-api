2882939399bdc8942006ad081b3724de6cca8d4e
areeburrub <areeburrub@gmail.com>
2023-01-05 20:16:49 +0530

    added webhook

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 
 export default function handler(req,res) {
-  res.status(200).json({ name: 'John Doe' })
+  //fetch some sample data from an external API usign axios
+  axios.get('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    console.log(response.data);
+    res.status(200).json({ data: response.data })
+  })
 }
