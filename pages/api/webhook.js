1a015592e59836423fbeec70eaf3de453ab69278
areeburrub <areeburrub@gmail.com>
2023-01-14 11:17:35 +0530

    debugging webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -29,6 +29,14 @@ export default function handler(req, res) {
   }
   else{
     console.log(JSON.stringify(req.body, null, 2));
+    if (req.body.object) {
+      if (
+        req.body.entry &&
+        req.body.entry[0].changes &&
+        req.body.entry[0].changes[0] &&
+        req.body.entry[0].changes[0].value.messages &&
+        req.body.entry[0].changes[0].value.messages[0]
+      ) {
     let phone_number_id =req.body.entry[0].changes[0].value.metadata.phone_number_id;
     let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
     let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
@@ -48,5 +56,11 @@ export default function handler(req, res) {
       console.log(error);
       res.status(400).json({ data: "success", error: error });
     });
+  }
+  res.send(200).json({ data: req.body });
+}
+else{
+  res.status(400).json({ data: "success", error: "no object" });
+}
   }
 }
