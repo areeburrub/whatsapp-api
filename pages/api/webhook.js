71919b7136345655f4acda563fe81034c0f4e305
areeburrub <areeburrub@gmail.com>
2023-01-14 10:51:55 +0530

    debugging webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -42,24 +42,26 @@ export default function handler(req, res) {
         req.body.entry[0].changes[0].value.messages &&
         req.body.entry[0].changes[0].value.messages[0]
       ) {
-        let phone_number_id =
-          req.body.entry[0].changes[0].value.metadata.phone_number_id;
+        let phone_number_id =req.body.entry[0].changes[0].value.metadata.phone_number_id;
         let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
         let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
         axios({
           method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-          url:
-            "https://graph.facebook.com/v12.0/" +
-            phone_number_id +
-            "/messages?access_token=" +
-            token,
+          url:"https://graph.facebook.com/v12.0/" +phone_number_id +"/messages?access_token=" +token,
           data: {
             messaging_product: "whatsapp",
             to: from,
             text: { body: "Ack: " + msg_body },
           },
           headers: { "Content-Type": "application/json" },
+        }).then((response) => {
+          console.log(response.data);
+          res.status(200).json({ data: "success", response: response.data });
+        }).catch((error) => {
+          console.log(error);
+          res.status(400).json({ data: "success", error: error });
         });
+
       }
       res.status(200).json({ data: 'success' });
     } else {
