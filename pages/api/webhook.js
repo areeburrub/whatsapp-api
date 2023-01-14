e78fc2bf05222651dc9ba172105b54b411422ea6
areeburrub <areeburrub@gmail.com>
2023-01-14 10:35:59 +0530

    debugging webhook

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -32,40 +32,39 @@ export default function handler(req, res) {
     console.log(req);
     // Check the Incoming webhook message
     console.log(JSON.stringify(req.body, null, 2));
-    res.status(200).json({ result: req.body });
 
-    // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
-    // if (req.body.object) {
-    //   if (
-    //     req.body.entry &&
-    //     req.body.entry[0].changes &&
-    //     req.body.entry[0].changes[0] &&
-    //     req.body.entry[0].changes[0].value.messages &&
-    //     req.body.entry[0].changes[0].value.messages[0]
-    //   ) {
-    //     let phone_number_id =
-    //       req.body.entry[0].changes[0].value.metadata.phone_number_id;
-    //     let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-    //     let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-    //     axios({
-    //       method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-    //       url:
-    //         "https://graph.facebook.com/v12.0/" +
-    //         phone_number_id +
-    //         "/messages?access_token=" +
-    //         token,
-    //       data: {
-    //         messaging_product: "whatsapp",
-    //         to: from,
-    //         text: { body: "Ack: " + msg_body },
-    //       },
-    //       headers: { "Content-Type": "application/json" },
-    //     });
-    //   }
-    //   res.status(200).json({ data: 'success' });
-    // } else {
-    //   // Return a '404 Not Found' if event is not from a WhatsApp API
-    //   res.status(404).json({ data: 'error' });
-    // }
+    //info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
+    if (req.body.object) {
+      if (
+        req.body.entry &&
+        req.body.entry[0].changes &&
+        req.body.entry[0].changes[0] &&
+        req.body.entry[0].changes[0].value.messages &&
+        req.body.entry[0].changes[0].value.messages[0]
+      ) {
+        let phone_number_id =
+          req.body.entry[0].changes[0].value.metadata.phone_number_id;
+        let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+        let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+        axios({
+          method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+          url:
+            "https://graph.facebook.com/v12.0/" +
+            phone_number_id +
+            "/messages?access_token=" +
+            token,
+          data: {
+            messaging_product: "whatsapp",
+            to: from,
+            text: { body: "Ack: " + msg_body },
+          },
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+      res.status(200).json({ data: 'success' });
+    } else {
+      // Return a '404 Not Found' if event is not from a WhatsApp API
+      res.status(404).json({ data: 'error' });
+    }
   }
 }
